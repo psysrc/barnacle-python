f123e475a7da97e0ac578752eb9a0616698e221b
psysrc <sam.copping@yahoo.com>
2022-04-15 15:57:13 +0100

    Hello World

---
+++ barnacle/__main__.py
@@ -0,0 +1,4 @@
+from barnacle.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='barnacle',
+   version='0.1.0',
+   description='An experimental interpreted programming language written in Python',
+   author='psysrc',
+   author_email='https://github.com/psysrc',
+   url='https://github.com/psysrc/barnacle-python',
+   packages=['barnacle'],
+   install_requires=['wheel']
+)
