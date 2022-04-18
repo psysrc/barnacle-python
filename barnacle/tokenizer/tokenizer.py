faac2b462cb7e785ad5e3967d4a983a0db695cba
psysrc <sam.copping@yahoo.com>
2022-04-18 14:01:18 +0100

    - Basic Tokenizer with support for whitespace, INTEGERS and STRINGS

--- barnacle/tokenizer/tokenizer.py
+++ barnacle/tokenizer/tokenizer.py
@@ -1,9 +1,43 @@
+"""
+tokenizer.py: Implements the Tokenizer class.
+
+The Tokenizer is responsible for the lexical analysis of the source code.
+It will produce a stream of tokens which can then undergo syntactic analysis.
+"""
+
+import re
+import logging
+from . import tokens
+
+
 class Tokenizer:
     def __init__(self, source: str):
         self.source = source
-        self.tmp = 5
+        logging.debug("Tokenizer initialised")
 
-    def next_token(self):
-        self.tmp -= 1
+    def next_token(self) -> dict:
+        if not self.has_more_tokens():
+            return None
 
-        return "Example token" if self.tmp > 0 else None
+        for regexp, token_type in tokens.TOKEN_REGEXPS.items():
+            match = re.search(regexp, self.source)
+
+            if match:
+                token_value = match.group()
+                self.source = self.source[len(token_value):]
+
+                if token_type:
+                    logging.debug(f"Tokenizer matched token '{token_value}' of type '{token_type}'")
+
+                    return {
+                        "type":  token_type,
+                        "value": token_value,
+                    }
+
+                else:
+                    return self.next_token()
+
+        raise SyntaxError(f"Unknown syntax near characters '{self.source[:10]}'")
+
+    def has_more_tokens(self) -> bool:
+        return bool(self.source)
---
+++ barnacle/tokenizer/tokens.py
@@ -0,0 +1,13 @@
+TOKEN_REGEXPS = {
+    # Key-value pairs represent a token regex and its corresponding token type
+    # A token type of None means the pseudo-token should be ignored (e.g. whitespace or comments)
+
+    # Whitespace (ignore)
+    "^\s": None,
+
+    # Integers
+    "^[0-9]+": "INTEGER",
+
+    # Strings
+    "^\"[^\"]*\"": "STRING",
+}
