ac0ce84daa11461bfdee40611c682e63a19dce3a
psysrc <sam.copping@yahoo.com>
2022-04-27 22:13:59 +0100

    - Hello World program now runs as expected - Basic interpreter which can understand print statements - Basic validation on AST nodes

--- barnacle/bcl_interpreter/interpreter.py
+++ barnacle/bcl_interpreter/interpreter.py
@@ -19,4 +19,41 @@ class Interpreter:
 
     def run(self):
         """Runs the Barnacle interpreter on the provided source."""
-        print(json.dumps(self.__ast, indent=4))
+        self.__interpret_program(self.__ast)
+
+    def __validate_node(self, ast: dict, expected_type: str, expected_keys: set):
+        """
+        Validates that the AST node has the expected type and contains the expected keys.
+
+        An AST node should always have a `type` key, so this does not need to be provided when calling this function.
+        """
+        expected_keys = expected_keys.union({"type"})
+
+        if "type" in ast.keys() and ast["type"] != expected_type:
+            actual_type = ast["type"]
+            raise RuntimeError(f"Node has unexpected type (expected '{expected_type}', got '{actual_type}')")
+
+        if set(ast.keys()) != expected_keys:
+            raise RuntimeError(f"Node does not contain the expected keys (expected '{expected_keys}', got '{set(ast.keys())}')")
+
+    def __interpret_program(self, ast: dict):
+        self.__validate_node(ast, "program", {"type", "body"})
+
+        for statement in ast["body"]:
+            self.__interpret_statement(statement)
+
+    def __interpret_statement(self, ast: dict):
+        # TODO: Implement this method properly
+        self.__interpret_print(ast)
+
+    def __interpret_print(self, ast: dict):
+        self.__validate_node(ast, "print", {"type", "body"})
+
+        string = self.__interpret_string_literal(ast["body"])
+
+        print(string)
+
+    def __interpret_string_literal(self, ast: dict) -> str:
+        self.__validate_node(ast, "string_literal", {"type", "value"})
+
+        return ast["value"]
