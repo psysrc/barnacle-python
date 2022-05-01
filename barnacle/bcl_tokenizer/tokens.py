5d5ca111ef1e97716d5713448428aa860b863f56
psysrc <sam.copping@yahoo.com>
2022-05-01 21:24:32 +0100

    Various pylint fixes

--- barnacle/bcl_tokenizer/tokens.py
+++ barnacle/bcl_tokenizer/tokens.py
@@ -1,45 +1,49 @@
+"""
+tokens.py: Contains the TOKEN_REGEXPS dictionary.
+"""
+
 TOKEN_REGEXPS = {
     # Key-value pairs represent a token regex and its corresponding token type
     # A token type of None means the pseudo-token should be ignored (e.g. whitespace or comments)
     # Order is important! Even if multiple regexes would match a token, the higher one in this dict takes precedence.
     # ==================== Ignore ====================
     # Whitespace (ignore)
-    "^\s": None,
+    r"^\s": None,
     # Block comments (ignore)
-    "^/\*(.|\n)*?\*/": None,
+    r"^/\*(.|\n)*?\*/": None,
     # Single-line comments (ignore)
-    "^//.*": None,
+    r"^//.*": None,
     # ==================== Literals ====================
     # Number literals
-    "^[0-9]+(\.[0-9]+)?": "NUMBER",
+    r"^[0-9]+(\.[0-9]+)?": "NUMBER",
     # String literals
-    '^"[^"]*"': "STRING",
+    r'^"[^"]*"': "STRING",
     # Boolean literals
-    "^true\s|^false\s": "BOOLEAN",
+    r"^true\s|^false\s": "BOOLEAN",
     # ==================== Keywords ====================
     # Keyword "let"
-    "^let\s": "LET",
+    r"^let\s": "LET",
     # Keyword "func"
-    "^func\s": "FUNC",
+    r"^func\s": "FUNC",
     # Keyword "print"
-    "^print\s": "PRINT",
+    r"^print\s": "PRINT",
     # Keyword "if"
-    "^if\s": "IF",
+    r"^if\s": "IF",
     # Keyword "else"
-    "^else\s": "ELSE",
+    r"^else\s": "ELSE",
     # ==================== Operators ====================
     # Assignment Operator
-    "^=": "ASSIGN_OP",
+    r"^=": "ASSIGN_OP",
     # ==================== Identifiers ====================
     # Identifiers (function names, variables, classes, etc)
-    "^[a-z][a-z0-9_]+": "IDENTIFIER",
+    r"^[a-z][a-z0-9_]+": "IDENTIFIER",
     # ==================== Miscellaneous ====================
     # Braces
-    "^{": "{",
-    "^}": "}",
+    r"^{": "{",
+    r"^}": "}",
     # Parentheses
-    "^\(": "(",
-    "^\)": ")",
+    r"^\(": "(",
+    r"^\)": ")",
     # Commas
-    "^,": ",",
+    r"^,": ",",
 }
