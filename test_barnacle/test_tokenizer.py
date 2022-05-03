3ebb64c4ebbdb4c20c29c29808cc26f19d7eef68
psysrc <sam.copping@yahoo.com>
2022-05-03 23:03:26 +0100

    Added a new tokenizer test and found a potential bug :)

--- test_barnacle/test_tokenizer.py
+++ test_barnacle/test_tokenizer.py
@@ -5,3 +5,10 @@ def test_empty():
     tokenizer = tkn.Tokenizer("")
     assert not tokenizer.has_more_tokens()
     assert tokenizer.next_token() is None
+
+def test_whitespace_only():
+    source = "         \n      \n\n\t\n \n \r\n \r      \r\n\r \t     \t \t\r\t\rn"
+    tokenizer = tkn.Tokenizer(source)
+
+    assert not tokenizer.has_more_tokens()
+    assert tokenizer.next_token() is None
