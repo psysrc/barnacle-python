0a83552c394bc90d02037f19058a47eb57485d04
psysrc <sam.copping@yahoo.com>
2022-05-04 21:52:51 +0100

    Improved helper functions in test_tokenizer.py

--- test_barnacle/test_tokenizer.py
+++ test_barnacle/test_tokenizer.py
@@ -5,8 +5,8 @@ test_tokenizer.py: Unit tests for the bcl_tokenizer submodule.
 from bcl_tokenizer import tokenizer as tkn
 
 
-def __verify_token(source: str, expected_token_type: str):
-    """Verify that the source produces exactly one token which has the expected token type when tokenized."""
+def __verify_token(source: str, expected_token: dict):
+    """Verify that the source produces exactly one token which matches the expected token when tokenized."""
 
     tokenizer = tkn.Tokenizer(source)
 
@@ -14,10 +14,21 @@ def __verify_token(source: str, expected_token_type: str):
 
     actual_token = tokenizer.next_token()
 
-    assert actual_token == {"type": expected_token_type, "value": source}
-
     assert tokenizer.end_of_stream()
     assert tokenizer.next_token() is None
+
+    assert actual_token == expected_token
+
+
+def __verify_token_type(source: str, expected_token_type: str):
+    """
+    Verify that the source produces exactly one token which, when tokenized:
+
+    - Has the expected token type.
+    - Has a value matching exactly the source.
+    """
+
+    __verify_token(source, {"type": expected_token_type, "value": source})
 
 
 def test_empty():
@@ -33,13 +44,8 @@ def test_whitespace():
     """Handling source that is only whitespace."""
 
     source = "         \n      \n\n\t\n \n \r\n \r      \r\n\r \t     \t \t\r\t\r\n"
-    tokenizer = tkn.Tokenizer(source)
 
-    assert not tokenizer.end_of_stream()
-
-    assert tokenizer.next_token() is None
-
-    assert tokenizer.end_of_stream()
+    __verify_token(source, None)
 
 
 def test_comments():
@@ -64,48 +70,42 @@ def test_comments():
 // Block comment inside a single-line comment /* hello */
 """
 
-    tokenizer = tkn.Tokenizer(source)
-
-    assert not tokenizer.end_of_stream()
-
-    assert tokenizer.next_token() is None
-
-    assert tokenizer.end_of_stream()
+    __verify_token(source, None)
 
 
 def test_number_literals():
     """Handling number literals."""
 
-    __verify_token("0", "NUMBER")
-    __verify_token("1", "NUMBER")
-    __verify_token("6", "NUMBER")
-    __verify_token("10", "NUMBER")
-    __verify_token("11", "NUMBER")
-    __verify_token("00", "NUMBER")
-    __verify_token("00000", "NUMBER")
-    __verify_token("05", "NUMBER")
-    __verify_token("024", "NUMBER")
-    __verify_token("298754", "NUMBER")
-    __verify_token("105", "NUMBER")
-    __verify_token("76485658457485", "NUMBER")
-    __verify_token("3286592", "NUMBER")
+    __verify_token_type("0", "NUMBER")
+    __verify_token_type("1", "NUMBER")
+    __verify_token_type("6", "NUMBER")
+    __verify_token_type("10", "NUMBER")
+    __verify_token_type("11", "NUMBER")
+    __verify_token_type("00", "NUMBER")
+    __verify_token_type("00000", "NUMBER")
+    __verify_token_type("05", "NUMBER")
+    __verify_token_type("024", "NUMBER")
+    __verify_token_type("298754", "NUMBER")
+    __verify_token_type("105", "NUMBER")
+    __verify_token_type("76485658457485", "NUMBER")
+    __verify_token_type("3286592", "NUMBER")
 
-    __verify_token("-1", "NUMBER")
-    __verify_token("-0", "NUMBER")
-    __verify_token("-13", "NUMBER")
-    __verify_token("-835623", "NUMBER")
-    __verify_token("-65987265795785", "NUMBER")
+    __verify_token_type("-1", "NUMBER")
+    __verify_token_type("-0", "NUMBER")
+    __verify_token_type("-13", "NUMBER")
+    __verify_token_type("-835623", "NUMBER")
+    __verify_token_type("-65987265795785", "NUMBER")
 
-    __verify_token("1.1", "NUMBER")
-    __verify_token("1.0", "NUMBER")
-    __verify_token("0.1", "NUMBER")
-    __verify_token("0.0", "NUMBER")
-    __verify_token("87.69", "NUMBER")
-    __verify_token("9832.7745756892246", "NUMBER")
-    __verify_token("000287650.0002865832000", "NUMBER")
+    __verify_token_type("1.1", "NUMBER")
+    __verify_token_type("1.0", "NUMBER")
+    __verify_token_type("0.1", "NUMBER")
+    __verify_token_type("0.0", "NUMBER")
+    __verify_token_type("87.69", "NUMBER")
+    __verify_token_type("9832.7745756892246", "NUMBER")
+    __verify_token_type("000287650.0002865832000", "NUMBER")
 
-    __verify_token("-0.1", "NUMBER")
-    __verify_token("-0.0", "NUMBER")
-    __verify_token("-87.69", "NUMBER")
-    __verify_token("-9832.7745756892246", "NUMBER")
-    __verify_token("-000287650.0002865832000", "NUMBER")
+    __verify_token_type("-0.1", "NUMBER")
+    __verify_token_type("-0.0", "NUMBER")
+    __verify_token_type("-87.69", "NUMBER")
+    __verify_token_type("-9832.7745756892246", "NUMBER")
+    __verify_token_type("-000287650.0002865832000", "NUMBER")
