fc4454ccb9a7a40149b9e4de4db4f071479f74ae
psysrc <sam.copping@yahoo.com>
2022-05-05 21:02:45 +0100

    - Added more 'empty' tests to test_parser.py - Added print tests to test_parser.py

--- test_barnacle/test_parser.py
+++ test_barnacle/test_parser.py
@@ -16,13 +16,97 @@ def __verify_ast(source: str, expected_ast: dict):
 
 
 def test_empty():
-    """Handling an empty source string."""
+    """Handling sources which do not contribute to the AST."""
 
-    source = ""
+    def __verify_empty_ast(source: str):
+        """Verify that the source produces an empty AST when parsed."""
+        __verify_ast(source, {"type": "program", "body": []})
 
-    expected_ast = {
-        "type": "program",
-        "body": [],
-    }
+    __verify_empty_ast("")
 
-    __verify_ast(source, expected_ast)
+    __verify_empty_ast("     \n  \n\n\n \t  \n \r\n \t\t\t\r\n\r\n   \t\n")
+
+    __verify_empty_ast("/* Just a block comment! */")
+
+    __verify_empty_ast("// Just a single line comment!")
+
+    __verify_empty_ast(
+        """\
+/* Just lots of comments and whitespace */
+\n\n\n\n\n \t\t\t\t \r\n\r\n\r\n
+// COMMENT        COMMENT       COMMENT
+/* Multi
+    Line
+    Comment
+*/
+"""
+    )
+
+
+def test_print():
+    """Handling print statements."""
+
+    __verify_ast(
+        source='print ""',
+        expected_ast={
+            "type": "program",
+            "body": [
+                {
+                    "type": "print",
+                    "body": {
+                        "type": "string_literal",
+                        "value": "",
+                    },
+                },
+            ],
+        },
+    )
+    __verify_ast(
+        source='print "Hello World!"',
+        expected_ast={
+            "type": "program",
+            "body": [
+                {
+                    "type": "print",
+                    "body": {
+                        "type": "string_literal",
+                        "value": "Hello World!",
+                    },
+                },
+            ],
+        },
+    )
+
+    __verify_ast(
+        source="""
+print "Hello World!"
+print "Second line!"
+print "Third line!!"
+""",
+        expected_ast={
+            "type": "program",
+            "body": [
+                {
+                    "type": "print",
+                    "body": {
+                        "type": "string_literal",
+                        "value": "Hello World!",
+                    },
+                },
+                {
+                    "type": "print",
+                    "body": {
+                        "type": "string_literal",
+                        "value": "Second line!",
+                    },
+                },
+                {
+                    "type": "print",
+                    "body": {
+                        "type": "string_literal",
+                        "value": "Third line!!",
+                    },
+                },
+            ],
+        },
+    )
